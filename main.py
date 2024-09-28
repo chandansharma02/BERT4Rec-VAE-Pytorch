e561bb248f319248a8221f1158763f43e8eb0b7f
Chandan Sharma <Chandan.Sharma@Axtria.com>
2024-09-28 15:44:15 +0530

    Commented part of main.py code

--- main.py
+++ main.py
@@ -10,13 +10,13 @@ from utils import *
 def train():
     export_root = setup_train(args)
     train_loader, val_loader, test_loader = dataloader_factory(args)
-    model = model_factory(args)
-    trainer = trainer_factory(args, model, train_loader, val_loader, test_loader, export_root)
-    trainer.train()
+    # model = model_factory(args)
+    # trainer = trainer_factory(args, model, train_loader, val_loader, test_loader, export_root)
+    # trainer.train()
 
-    test_model = (input('Test model with test dataset? y/[n]: ') == 'y')
-    if test_model:
-        trainer.test()
+    # test_model = ('y' == 'y')
+    # if test_model:
+    #     trainer.test()
 
 
 if __name__ == '__main__':
